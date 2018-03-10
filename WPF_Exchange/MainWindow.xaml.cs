54a7c3c218d42f027573286cb338ec2ab8745628
Szymon Bortel <szymonbortel8@gmail.com>
2018-03-10 20:27:01 +0100

    Initial commit - działająca aplikacja dla najbardziej popularnych walut

--- WPF_Exchange/MainWindow.xaml.cs
+++ WPF_Exchange/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace WPF_Exchange
 {
@@ -23,6 +24,46 @@ namespace WPF_Exchange
         public MainWindow()
         {
             InitializeComponent();
+         
+        }
+
+        void GetData()
+        {        
+                XmlDocument kurs = new XmlDocument();
+                kurs.Load("http://www.nbp.pl/kursy/xml/a049z180309.xml");   //ladowanie danych z NBP
+
+                XmlNodeList elemList = kurs.GetElementsByTagName("nazwa_waluty");
+                XmlElement elemList3 = kurs.GetElementById("forint (Węgry)");
+                XmlNodeList elemList2 = kurs.GetElementsByTagName("kurs_sredni");
+            
+            dolary.Text = search(elemList, elemList2,"dolar amerykański") + " PLN";
+            euro.Text = search(elemList, elemList2, "euro") + " PLN";
+            franki.Text = search(elemList, elemList2, "frank szwajcarski") + " PLN";
+            funt.Text = search(elemList, elemList2, "funt szterling") + " PLN";
+            korony.Text = search(elemList, elemList2, "korona czeska") + " PLN";
+            ruble.Text = search(elemList, elemList2, "rubel rosyjski") + " PLN";
+          
+        }
+
+        string search(XmlNodeList listwaluta,XmlNodeList kurs, string waluta)
+        {
+            for (int i = 0; i < listwaluta.Count; i++)
+            {
+                if (listwaluta[i].InnerXml.ToString() == waluta)
+                {                     
+                    return kurs[i].InnerXml.ToString();
+                }   
+            }
+            return "ERROR";
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GetData();
         }
     }
+
 }
+
+
